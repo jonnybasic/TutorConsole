9a7d262a5329ad58bfad2571c0114df1d4752c1e
Jonny Basic <jonnybasic@hotmail.com>
2018-04-01 23:47:52 -0400

    Used new GameEngine class

--- main.cpp
+++ main.cpp
@@ -1,57 +1,128 @@
 
 #include <iostream>
+#include <sstream>
+#include <string>
 
 #include "ANSIGameInput.h"
 #include "ANSIGameOutput.h"
 
+#include "GameEngine.h"
+
 using std::cout;
 using std::cerr;
 using std::endl;
+using std::string;
+using std::stringstream;
 
 const Color BLACK(0, 0, 0);
 const Color WHITE(255, 255, 255);
 
+class MyGame : public GameEngine
+{
+public:
+	MyGame() 
+		: GameEngine::GameEngine(new ANSIGameInput(), new ANSIGameOutput()),
+		m_InfoBuffer()
+	{ }
+
+	virtual ~MyGame()
+	{ }
+
+protected:
+	virtual bool initialize(IGameInput *input, IGameOutput *output);
+	virtual void shutdown();
+	virtual void update(double frameTime);
+	virtual void updateInput(const IGameInput *input);
+	virtual void updateOutput(IGameOutput *output, double frameTime);
+	virtual void updateWindow(int width, int height);
+
+private:
+	int m_ScreenHeight;
+	int m_ScreenWidth;
+	byte m_LastInput;
+	string m_InfoBuffer;
+};
+
+bool MyGame::initialize(IGameInput *input, IGameOutput *output)
+{
+	// run at 15Hz
+	setUpdateRate(15.0);
+	return true;
+}
+
+void MyGame::shutdown()
+{
+
+}
+
+void MyGame::update(double frameTime)
+{
+	//m_InfoBuffer.clear();
+}
+
+void MyGame::updateInput(const IGameInput *input)
+{
+	// clear info buffer
+	m_InfoBuffer = "";
+	byte userInput = 0;
+	if (input->tryGetInput(userInput))
+	{
+		auto ss = stringstream();
+		// store last input
+		m_LastInput = userInput;
+		switch (userInput)
+		{
+		case 'q':
+			setIsRunning(false);
+			break;
+		}
+		// write to the info buffer
+		ss << "KEY_" << (int)m_LastInput << "    ";
+		m_InfoBuffer = ss.str();
+	}
+}
+
+void MyGame::updateOutput(IGameOutput *output, double frameTime)
+{
+	const auto ansiOutput = static_cast<ANSIGameOutput *>(output);
+
+	// Clear screen
+	//ansiOutput->clear();
+
+	// Draw a border around the screen
+	Color borderColor(0, 255, 255);
+	ansiOutput->setForeColor(borderColor);
+	ansiOutput->drawBox(0, 0, m_ScreenWidth, m_ScreenHeight);
+	ansiOutput->reset();
+
+	// Draw a title
+	Color red(255, 0, 0);
+	ansiOutput->setForeColor(red);
+	ansiOutput->drawBox(4, 4, 15, 3);
+	ansiOutput->moveTo(6, 5);
+	ansiOutput->print("Hello World");
+	ansiOutput->reset();
+
+	// Draw instructions
+	ansiOutput->moveTo(2, m_ScreenHeight - 4);
+	ansiOutput->print("Instructions.");
+	ansiOutput->moveTo(3, m_ScreenHeight - 3);
+	ansiOutput->print("-hit 'q' to exit-");
+	ansiOutput->moveTo(3, m_ScreenHeight - 2);
+	ansiOutput->print(m_InfoBuffer.c_str());
+}
+
+void MyGame::updateWindow(int width, int height)
+{
+	m_ScreenHeight = height;
+	m_ScreenWidth = width;
+}
+
 int main(int argc, char** argv) 
 {
-	auto gameInput = ANSIGameInput();
-	auto gameOutput = ANSIGameOutput();
+	auto game = MyGame();
 
-	if (gameInput.initialize() 
-		&& gameOutput.initialize())
-	{
-		int height;
-		int width;
-		gameOutput.getViewport(width, height);
+	game.run();
 
-		// Draw a border around the screen
-		Color borderColor(0, 255, 255);
-		gameOutput.setForeColor(borderColor);
-		gameOutput.drawBox(0, 0, width, height);
-		gameOutput.reset();
-
-		// Draw a title
-		Color red(255, 0, 0);
-		gameOutput.setForeColor(red);
-		gameOutput.drawBox(4, 4, 15, 3);
-		gameOutput.moveTo(6, 5);
-		gameOutput.print("Hello World");
-		gameOutput.reset();
-
-		// Draw instructions
-		gameOutput.moveTo(2, height - 3);
-		gameOutput.print("Instructions.");
-		gameOutput.moveTo(3, height - 2);
-		gameOutput.print("-hit any key to exit-");
-		
-		auto inChar = gameInput.getInput();
-
-		gameOutput.shutdown();
-		gameInput.shutdown();
-		return 0;
-	}
-	else
-	{
-		cerr << "Unable to initialize engine!" << endl;
-		return -1;
-	}
+	return 0;
 }
